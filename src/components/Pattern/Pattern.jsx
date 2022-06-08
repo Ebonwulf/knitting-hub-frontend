ccd56bf3ba425e1284bdea3ac7b128c0739a653a
Lyssa <ebonwolf00@gmail.com>
2022-06-08 17:14:21 +0100

    scss changes made to search box to improve the layout

--- src/components/Pattern/Pattern.jsx
+++ src/components/Pattern/Pattern.jsx
@@ -17,7 +17,7 @@ const Pattern = (props) => {
         <h3>{patternName}</h3>
         <h4>Pattern type: {patternType}</h4>
         <h5>Difficulty: {patternDifficulty}</h5>
-        <h6>Wool type: {woolType}</h6>
+        <h5>Wool type: {woolType}</h5>
         <h6>Needle/hook size: {needleSize}</h6>
         <p>Description: {patternDescription}</p>
         <Link to='/pattern-page'>Go to pattern</Link>

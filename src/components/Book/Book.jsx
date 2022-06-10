2e55d0b33f4f3b527852e915f7f1c598c469fe78
Lyssa <ebonwolf00@gmail.com>
2022-06-10 11:03:55 +0100

    book.jsx component completed

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,7 +1,36 @@
 import './Book.scss';
 
-const Book = () => {
-  return <div>Book</div>;
+const Book = (props) => {
+  const { bookId, bookTitle, bookGenre, bookAuthor, bookDescription } =
+    props.book;
+
+  const handleDelete = () => {
+    fetch('https://wolfs-knitting-hub-5h356xxfoq-nw.a.run.app/book/' + bookId, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.log(err));
+    console.log('deleted');
+  };
+
+  return (
+    <div className='book'>
+      <div className='book__btn-container'>
+        <h2>{bookTitle}</h2>
+        <button onClick={handleDelete} className='book__btn'>
+          Del
+        </button>
+      </div>
+      <h3>Author: {bookAuthor}</h3>
+      <h4>Genre: {bookGenre}</h4>
+      <p>Description: {bookDescription}</p>
+      <Link to='/book-page'>Go to full book description</Link>
+    </div>
+  );
 };
 
 export default Book;

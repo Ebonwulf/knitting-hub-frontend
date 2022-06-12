dae3786477c85b62ebd57bc4ba7fc4ebb43bc7c9
Lyssa <ebonwolf00@gmail.com>
2022-06-12 11:18:51 +0100

    book list folder page, jsc and scss created and completed

---
+++ src/pages/BookList/BookList.jsx
@@ -0,0 +1,31 @@
+import './BookList.scss';
+import Book from '../../components/Book/Book';
+
+const BookList = ({ filteredBooks, toggleSearch, showSearch }) => {
+  return (
+    <>
+      <div className='book-list'>
+        {showSearch && <SearchBar toggleSearch={toggleSearch} />}
+        <h2 className='book-list__heading'>Book Library</h2>
+        <div className='search-bar__button' onClick={toggleSearch}>
+          Book Finder
+        </div>
+        <div className='book-list__cont'>
+          {filteredBooks &&
+            filteredBooks.map((book) => (
+              <Book
+                key={book.bookTitle}
+                bookTitle={book.bookTitle}
+                bookId={book.bookId}
+                bookGenre={book.bookGenre}
+                bookAuthor={book.bookAuthor}
+                bookDescription={book.bookDescription}
+              />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BookList;

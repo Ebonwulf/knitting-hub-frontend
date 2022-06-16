5c9c7a4e48445ccffa35fdb756109ec61e7aad93
Lyssa <ebonwolf00@gmail.com>
2022-06-16 10:37:29 +0100

    path names updated in the switch statement in main.jsx to get the nav bar working correctly after making changes

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,3 +1,4 @@
+import './Main.scss';
 import Dashboard from '../../containers/Dashboard/Dashboard';
 import BookDashboard from '../../containers/BookDashboard/BookDashboard';
 import PatternDashboard from '../../containers/PatternDashboard/PatternDashboard';
@@ -5,19 +6,19 @@ import PatternDashboard from '../../containers/PatternDashboard/PatternDashboard
 const Main = () => {
   const changeNavBar = () => {
     switch (window.location.pathname) {
-      case '/':
+      case '/knitting-hub-frontend/':
         return <Dashboard refreshPage={refreshPage} />;
-      case '/book-hub':
+      case '/knitting-hub-frontend/book-hub':
         return <BookDashboard refreshPage={refreshPage} />;
-      case '/books':
+      case '/knitting-hub-frontend/books':
         return <BookDashboard refreshPage={refreshPage} />;
-      case '/new-book':
+      case '/knitting-hub-frontend/new-book':
         return <BookDashboard />;
-      case '/pattern-hub':
+      case '/knitting-hub-frontend/pattern-hub':
         return <PatternDashboard refreshPage={refreshPage} />;
-      case '/patterns':
+      case '/knitting-hub-frontend/patterns':
         return <PatternDashboard refreshPage={refreshPage} />;
-      case '/new-pattern':
+      case '/knitting-hub-frontend/new-pattern':
         return <PatternDashboard />;
       default:
         return <Dashboard refreshPage={refreshPage} />;

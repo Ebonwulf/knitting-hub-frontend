ea51c99b92666da5909fed6b9ce39cc2972ffe06
Lyssa <ebonwolf00@gmail.com>
2022-06-28 17:23:41 +0100

    form styling changed to make the box sit better on the screen and the div surrounding the forms have been removed as it was unnessassary

---
+++ src/components/FullPattern/FullPattern.jsx
@@ -0,0 +1,7 @@
+import './FullPattern.scss';
+
+const FullPattern = () => {
+  return <div>FullPattern</div>;
+};
+
+export default FullPattern;
--- src/pages/BookForm/BookForm.jsx
+++ src/pages/BookForm/BookForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './BookForm.scss';
 
 const BookForm = () => {
   const [book, setBook] = useState({
